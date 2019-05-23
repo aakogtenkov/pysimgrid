2ddab597989c18b77d594bfd5eac231952210840
Алексей <alex_kogtenkov@mail.ru>
2019-05-23 21:23:52 +0300

    ScaleEstimator added

--- pysimgrid/tools/estimator.py
+++ pysimgrid/tools/estimator.py
@@ -33,3 +33,23 @@ class SimpleDispersionEstimator(Estimator):
         low = max(float(value * (1. - self.percentage)), 1.0)
         high = max(float(value * (1. + self.percentage)), 1.0)
         return np.random.uniform(low, high)
+
+
+class ScaleEstimator(Estimator):
+    def __init__(self, scale, seed=1234):
+        self.scale = scale
+        if self.scale < 1:
+            self.scale = 1 / self.scale
+        np.random.seed(seed)
+        super().__init__()
+
+    def generate(self, value):
+        prob_le = self.scale / (self.scale + 1)
+        is_le = np.random.uniform(0, 1)
+        is_le = (is_le <= prob_le)
+        if is_le:
+            res = np.random.uniform(value / self.scale, value)
+        else:
+            res = np.random.uniform(value, value * self.scale)
+        res = max(1, res)
+        return res
